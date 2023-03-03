285a1140ba4d9ab20a6bc0fa70976ef31d951ea8
Jesus Manuel Paredon Rivera <jmparedonr2005@gmail.com>
2023-03-03 12:41:03 -0600

    Style. Maquetado // 50%

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -34,9 +34,24 @@ const Navbar = () => {
             </div>
 
             <div className="conteinerBody flex items-center w-3/4 h-full">
+
                 <div className='w-full h-6/7 mr-1/7 bg-gray-700'>
+
                     <div className="txt1 h-1/7 flex items-center justify-center bg-orange-300 text-green text-3xl font-semibold">Citas</div>
+
+                    <div className="conteiner4 flex flex-row justify-around bg-slate-900 w-10/12 h-2/5 ">
+                        <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales usuarios</div>
+                        <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales de animales</div>
+                        <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales en el día</div>
+                        <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Promedio de citas en el mes</div>
+                    </div>
+
+                    <div className="graph1 flex">
+                        <div>Hola</div>
+                    </div>
+
                 </div>
+
             </div>
 
             {/* <div className="conteinerBody flex items-center w-3/4 h-full">
