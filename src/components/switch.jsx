42952aa16d99ca84708fc80f509fb9b9c0665921
Elgussgood <gsuarezr2000@alumno.ipn.mx>
2023-03-06 14:51:40 -0600

    Style Switch funtion

--- src/components/switch.jsx
+++ src/components/switch.jsx
@@ -7,10 +7,10 @@ import { Link } from 'react-router-dom'
 const Switch = ({Mode, Click}) => {
     return (
         <div className="switch_conteiner h-[10%] w-full mb-[2%]">
-            <div className='w-[15%] h-[100%] ml-[5%] bg-[#6ED4A5] p-[.5%] rounded-[3vw] items-center content-center'>
-                <button className='outline-none w-full h-full m-0 p-0 bg-trasparent' onClick={{Click}}>
-                    <div className='h-full w-full bg-white rounded-[3vw] p-[5%] '> 
-                        <div className={"h-[6vh] w-[3.5vw] bg-[#6ED4A5] rounded-[50%] "+ Mode}></div>
+            <div className='w-[15%] h-[100%] ml-[5%] bg-[#6ED4A5] p-[.3%] rounded-[3vw] items-center content-center'>
+                <button className='outline-none w-full h-full m-0 p-0 bg-trasparent' onClick={Click}>
+                    <div className='h-full w-full bg-white rounded-[3vw] py-[1vh] px-[.4vw] flex'> 
+                        <div className={"h-[6vh] w-[2.8vw] bg-[#6ED4A5] rounded-[50%] " + Mode}></div>
                     </div>
                 </button>
             </div>
--- src/pages/dates/index.jsx
+++ src/pages/dates/index.jsx
@@ -5,7 +5,7 @@ import Switch from '../../components/switch'
 const DatesDash = () => {
 
     //Gráfica de citas por día en el mes
-    const [Mode, setMode]= useState(false)
+    const [Mode, setMode]= useState(true)
     const [series, setSeries]= useState([{
         name: 'Dia',
         data: [31, 40, 28, 51, 42, 109, 100]
@@ -251,12 +251,15 @@ const DatesDash = () => {
                     <div className='w-full h-6/7'>
 
                         <div className="h-1/7 flex items-center justify-center text-green text-3xl font-semibold">
-                            Citas
+                            Citas datos
                         </div>
 
-                        <div className="switch_conteiner h-[12%]">
-                            <div className='AQUIVA_TU_PERRO_SWITCH w-full'>Switch</div>
-                        </div>
+                        <Switch
+                        Mode={"rightswitch"}
+                        Click={()=>{
+                            Mode? setMode(false) : setMode(true)
+                          }}
+                        />
                         
                         <div className="graphs overflow-y-scroll w-[100%] h-[73%]">
 
@@ -319,116 +322,14 @@ const DatesDash = () => {
                             </div>
 
                         </div>
-                            
-
-                            {/* <div>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui obcaecati nisi maxime vel aut nobis, natus similique, provident itaque exercitationem expedita dignissimos harum aliquam iusto perspiciatis placeat laboriosam, numquam necessitatibus? 
-                            </div> */}
-                        
-
-                        {/* <div className="conteiner4 flex flex-row justify-around bg-slate-900 w-10/12 h-2/5 ">
-                            <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales usuarios</div>
-                            <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales de animales</div>
-                            <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales en el día</div>
-                            <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Promedio de citas en el mes</div>
-                        </div> */}
-
                     </div>
                     
                 </div>
                 
         </div>
         ):(
-<div className='w-screen h-screen flex'>
+        <div className='w-screen h-screen flex'>
             <Navbar/>
-
-            <div className="flex items-center w-[85%] h-full">
-
-                    <div className='w-full h-6/7'>
-
-                        <div className="h-1/7 flex items-center justify-center text-green text-3xl font-semibold">
-                            Citas
-                        </div>
-                        <Switch/>
-                    <div className="graphs overflow-y-scroll w-[100%] h-[73%]">
-
-                            <div className=' flex w-[95%] h-[73%] justify-around mt-[5.5%] ml-[2.3%] '>
-
-                                <div className="chart1 w-[46%] bg-[#D9D9D9] rounded-1/7">
-                                    <div className='mt-[2%]'>
-                                        <Chart 
-                                            series={series} 
-                                            options={options} 
-                                            type='area'
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="chart2 w-[46%] bg-[#D9D9D9] rounded-1/7">
-                                    <div className='mt-[2%]'>
-                                        <Chart 
-                                            series={series2}
-                                            options={options2}
-                                            type='bar'
-                                        />
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className=' w-full h-[122%] flex items-center justify-center'>
-
-                                <div className='chart3 w-[55%] bg-[#D9D9D9] rounded-1/7'>
-                                    <div className='mt-[2%]'>
-                                        <Chart
-                                            series={series3}
-                                            options={options3}
-                                            type='bar'
-                                        />
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className='w-full h-full flex justify-around'>
-
-                                <div className='chart4 w-[40%] h-[85%]  bg-[#D9D9D9] rounded-1/7'>
-                                    <div className='mt-[2%]'>
-                                        <Chart 
-                                            series={series4}
-                                            options={options4}
-                                            type='polarArea'
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className='chart5 w-[40%] h-[85%]  bg-[#D9D9D9] rounded-1/7'>
-                                    <div className='mt-[2%]'>
-                                        
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </div>
-                            
-
-                            {/* <div>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui obcaecati nisi maxime vel aut nobis, natus similique, provident itaque exercitationem expedita dignissimos harum aliquam iusto perspiciatis placeat laboriosam, numquam necessitatibus? 
-                            </div> */}
-                        
-
-                        {/* <div className="conteiner4 flex flex-row justify-around bg-slate-900 w-10/12 h-2/5 ">
-                            <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales usuarios</div>
-                            <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales de animales</div>
-                            <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Citas totales en el día</div>
-                            <div className='bg-red-400 w-1/5 rounded-1/7 h-full'>Promedio de citas en el mes</div>
-                        </div> */}
-
-                    </div>
-                    
-                </div>
-                
         </div>
         )
 
